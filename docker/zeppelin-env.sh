474b6f4a1ebcf3fdd3ff965ab74d9cb9abe4ea1b
Sebastian Woehrl <sebastian.woehrl@maibornwolff.de>
2018-01-27 11:16:34 +0100

    New env variable SPARK_EXECUTOR_CORES

--- docker/zeppelin-env.sh
+++ docker/zeppelin-env.sh
@@ -3,17 +3,22 @@ export ZEPPELIN_INTP_JAVA_OPTS="${ZEPPELIN_INTP_JAVA_OPTS} -Dspark.mesos.coarse=
 
 if [ -n "${SPARK_MESOS_EXECUTOR_DOCKER_IMAGE}" ]; then
   export ZEPPELIN_JAVA_OPTS="${ZEPPELIN_JAVA_OPTS} -Dspark.mesos.executor.docker.image=${SPARK_MESOS_EXECUTOR_DOCKER_IMAGE}"
-  export ZEPPELIN_INTP_JAVA_OPTS="${ZEPPELIN_JAVA_OPTS} -Dspark.mesos.executor.docker.image=${SPARK_MESOS_EXECUTOR_DOCKER_IMAGE}"
+  export ZEPPELIN_INTP_JAVA_OPTS="${ZEPPELIN_INTP_JAVA_OPTS} -Dspark.mesos.executor.docker.image=${SPARK_MESOS_EXECUTOR_DOCKER_IMAGE}"
 fi
 
 if [ -n "${SPARK_EXECUTOR_MEMORY}" ]; then
   export ZEPPELIN_JAVA_OPTS="${ZEPPELIN_JAVA_OPTS} -Dspark.executor.memory=${SPARK_EXECUTOR_MEMORY}"
-  export ZEPPELIN_INTP_JAVA_OPTS="${ZEPPELIN_JAVA_OPTS} -Dspark.executor.memory=${SPARK_EXECUTOR_MEMORY}"
+  export ZEPPELIN_INTP_JAVA_OPTS="${ZEPPELIN_INTP_JAVA_OPTS} -Dspark.executor.memory=${SPARK_EXECUTOR_MEMORY}"
 fi
 
 if [ -n "${SPARK_CORES_MAX}" ]; then
   export ZEPPELIN_JAVA_OPTS="${ZEPPELIN_JAVA_OPTS} -Dspark.cores.max=${SPARK_CORES_MAX}"
-  export ZEPPELIN_INTP_JAVA_OPTS="${ZEPPELIN_JAVA_OPTS} -Dspark.cores.max=${SPARK_CORES_MAX}"
+  export ZEPPELIN_INTP_JAVA_OPTS="${ZEPPELIN_INTP_JAVA_OPTS} -Dspark.cores.max=${SPARK_CORES_MAX}"
+fi
+
+if [ -n "${SPARK_EXECUTOR_CORES}" ]; then
+  export ZEPPELIN_JAVA_OPTS="${ZEPPELIN_JAVA_OPTS} -Dspark.executor.cores=${SPARK_EXECUTOR_CORES}"
+  export ZEPPELIN_INTP_JAVA_OPTS="${ZEPPELIN_INTP_JAVA_OPTS} -Dspark.executor.cores=${SPARK_EXECUTOR_CORES}"
 fi
 
 export MASTER=mesos://zk://master.mesos:2181/mesos
