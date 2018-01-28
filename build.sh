1b1130430b5aa572d666676998fdbecea044f813
Sebastian Woehrl <sebastian.woehrl@maibornwolff.de>
2018-01-28 11:17:02 +0100

    New release

--- build.sh
+++ build.sh
@@ -1,18 +1,18 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-SPARK_VERSIONS="2.1.0-2.2.1-1-hadoop-2.6 2.1.0-2.2.1-1-hadoop-2.7"
+declare -A spark_versions=( ["2.2.1-1-hadoop-2.6"]="2.1.0-2.2.1-1-hadoop-2.6" ["2.2.1-1-hadoop-2.7"]="2.1.0-2.2.1-1-hadoop-2.7" )
 ZEPPELIN_VERSION=0.7.3
+RELEASE=${RELEASE:-2}
 DOCKER_REPO=${DOCKER_REPO:-maibornwolff}
-MINOR_VERSION=1
 
 cd docker
-for version in $SPARK_VERSIONS; do
+for version in "${!spark_versions[@]}"; do
   for package in all netinst; do
-    echo "Building zeppelin:$ZEPPELIN_VERSION-$version-$package"
-    docker build -t zeppelin:$ZEPPELIN_VERSION-$version-$package-$MINOR_VERSION --build-arg SPARK_VERSION=$version --build-arg ZEPPELIN_VERSION=$ZEPPELIN_VERSION --build-arg PACKAGE_TYPE=$package .
+    echo "Building zeppelin:$ZEPPELIN_VERSION-$RELEASE-$package-$version"
+    docker build -t zeppelin:$ZEPPELIN_VERSION-$RELEASE-$package-$version --build-arg SPARK_VERSION=${spark_versions[$version]} --build-arg ZEPPELIN_VERSION=$ZEPPELIN_VERSION --build-arg PACKAGE_TYPE=$package .
     if [ ${PUSH:-"0"} == "1" ]; then
-      docker tag zeppelin:$ZEPPELIN_VERSION-$version-$package-$MINOR_VERSION $DOCKER_REPO/zeppelin:$ZEPPELIN_VERSION-$version-$package-$MINOR_VERSION
-      docker push $DOCKER_REPO/zeppelin:$ZEPPELIN_VERSION-$version-$package-$MINOR_VERSION
+      docker tag zeppelin:$ZEPPELIN_VERSION-$RELEASE-$package-$version $DOCKER_REPO/zeppelin:$ZEPPELIN_VERSION-$RELEASE-$package-$version
+      docker push $DOCKER_REPO/zeppelin:$ZEPPELIN_VERSION-$RELEASE-$package-$version
     fi
   done
 done
